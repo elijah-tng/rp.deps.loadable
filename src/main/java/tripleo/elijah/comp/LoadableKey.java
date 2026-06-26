4be1318f89ed14b44c2117b630007884bdcdb02e
tripleo1 <50311859+tripleo1@users.noreply.github.com>
2026-06-26 21:06:21 +0000

    1st POC: slightly vibeby, lgtm

--- src/main/java/tripleo/elijah/comp/LoadableKey.java
+++ src/main/java/tripleo/elijah/comp/LoadableKey.java
@@ -1,13 +1,13 @@
 package tripleo.elijah.comp;
 
+import java.lang.reflect.Type;
+
 import io.activej.inject.Key;
 import io.activej.types.Types;
 
-import java.lang.reflect.Type;
-
 /// TODO we left activej-inject to get {@link io.activej.inject.Key}
 public record LoadableKey(Class<?> klazz) {
-	public <T> io.activej.inject.Key<Class<T>> getKey() {
+	public <T> Key<Class<T>> getKey() {
 		final Type pt = Types.parameterizedType(klazz);
 		return Key.ofType(pt);
 	}
